e8e5a409da3d2a45eb7388fb4eac33bbe2ca23bd
koku17 <someone@domain.com>
2024-04-26 19:34:33 +0530

    update auto.sh (automaiton script), makefile

--- 4/ADA/Examples/auto.sh
+++ 4/ADA/Examples/auto.sh
@@ -4,17 +4,33 @@ j=1
 
 while true
 do
-	clear
+	tput -x clear
 	echo "Time Complexity Test ($j)"
+
+	# Number of tests
 	j=$(( j+1 ))
+
+	# Generate RANDOM variable
 	p=$RANDOM
 	q=$RANDOM
+
+	# Execute 3 algorithm separately
 	for i in e m c
 	do
 		printf "\n"
 		time ./gcd.bin $i $p $q
 	done
+
+	# Verification
 	echo -e "\nVerification"
-	printf "qalc 'gcd($p,$q)' = $(qalc -t "gcd($p,$q)")"
+	printf "qalc 'gcd($p,$q)' = $(qalc -t "gcd($p,$q)")\n\n"
+
+	# Verify each algorithms
+	[ $(./gcd.bin e $p $q | \grep "=" | cut -d "=" -f 2) -eq $(qalc -t "gcd($p,$q)") ] || \
+		echo "Verification failed for Euclidean Algorithms !"
+	[ $(./gcd.bin m $p $q | \grep "=" | cut -d "=" -f 2) -eq $(qalc -t "gcd($p,$q)") ] || \
+		echo "Verification failed for Mid-School Algorithms !"
+	[ $(./gcd.bin c $p $q | \grep "=" | cut -d "=" -f 2) -eq $(qalc -t "gcd($p,$q)") ] || \
+		echo "Verification failed for CIC Algorithms !"
 	sleep 3
 done
