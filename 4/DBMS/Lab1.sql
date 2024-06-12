e8a45dbf5f5f29a3c1695e183770615bfbef8a7c
Hacker <youremail@yourdomain.com>
2024-06-12 21:52:57 +0530

    update DBMS and ADA

--- 4/DBMS/Lab1.sql
+++ 4/DBMS/Lab1.sql
@@ -1,32 +1,28 @@
-# Create User and grant all permissions
+-- Create User and grant all permissions
 create user 'tester'@'localhost' identified by 'tester';
 grant all privileges on *.* to 'tester'@'localhost';
 flush privileges;
 show grants for 'tester'@'localhost';
 
-# Create db
+-- Create db
 create database employee;
 use employee;
 show databases;
 
-# Create Table
+-- Create Table
 create table Employee(
 	Empno int,Ename varchar(20),Job varchar(20),Manager_No int,Salary Decimal(10,2),Commission int
 );
 
-# Get table info
+-- Get table info
 desc Employee;
 
-# Insert data into table
+-- Insert data into table
 insert into Employee values(
 	100,'Abhishek','Manager',10,25000,70
-);
-
-insert into Employee values(
+),(
 	101,'Bharat','Analyst',20,20000,60
-);
-
-insert into Employee values(
+),(
 	102,'Charan','Trainee',30,15000,50
 );
 
@@ -50,7 +46,7 @@ select * from Employee;
 rollback;
 select * from Employee;
 
-# Primary Key
+-- Primary Key
 alter table Employee add primary key(Empno), modify Ename varchar(20) NOT NULL;
 desc Employee;
 
--- 4/DBMS/Lab2.sql
+++ 4/DBMS/Lab2.sql
@@ -12,27 +12,20 @@ create table Employees(
 desc employees
 
 alter table employees add(commission int);
-
 insert into Employee values(
         100,'Abhishek','Manager',10,25000
-);
-
-insert into Employee values(
+),(
         101,'Bharat','Analyst',20,20000
-);
-
-insert into Employee values(
+),(
         102,'Charan','Trainee',30,15000
 );
 
 select * from Employees;
 
-update Employees set Job='Trainee' where empno=101;
-
-alter table employees rename column mgr to manager_no;
+update Employees set Job='Trainee' where Empno=101;
+alter table employees rename column MGR to Manager_No;
 
 select * from Employees;
 
-delete from Employees where empno=105;
-
+delete from Employees where Empno=105;
 select * from Employees;
